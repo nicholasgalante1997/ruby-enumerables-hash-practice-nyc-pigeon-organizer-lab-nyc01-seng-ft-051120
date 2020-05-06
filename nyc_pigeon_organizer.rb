c3a4500871e4cdc46b02f51f0adb7063ce888bcd
Nicholas Galante <ngalant2@binghamton.edu>
2020-05-06 12:11:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,9 @@ def nyc_pigeon_organizer(data)
   final_hash = {} 
   data.each do |first_level, other_first|
     other_first.each do |category, array|
+    array.each do |name|
     
+    end 
     end 
   end
 end
